f7e628af6420eaffa09067c1f57a1d9d1a4b61bc
Badal Agarwal <badal.agarwal1369@gmail.com>
2022-08-03 14:15:50 +0530

    Initial Setup and Basic UI done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useEffect } from 'react';
+import { StreamChat } from 'stream-chat';
+import { OverlayProvider, Chat }  from 'stream-chat-expo';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+const API_KEY = "scyh67pb6f4y";
+const client = StreamChat.getInstance(API_KEY);
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+      const connectUser = async () => {
+
+        await client.connectUser(
+          {
+            id: 'badal',
+            name: 'Badal Agarwal',
+            image: 'https://i.imgur.com/fR9Jz14.png'
+          },
+          client.devToken('badal'),
+        );
+
+        // Create a Channel
+
+        const channel =  client.channel("messaging", "thescrambler", {name: "theScrambler"} );
+        await channel.watch();
+      
+      };
+
+      connectUser();
+
+      return () => { client.disconnectUser(); }
+
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+
+        <OverlayProvider>
+          <Chat client = {client}>
+            { /* <Navigation colorScheme={colorScheme} /> */ }
+            
+            
+          </Chat>
+        </OverlayProvider>
+
         <StatusBar />
       </SafeAreaProvider>
     );
